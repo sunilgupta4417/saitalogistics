d870251ba3a17c09f7a460aa28b6df8ec74dea8e
Shivkumar <acharyashivkumar67@gmail.com>
2023-03-30 11:15:20 +0530

    only user role allowed to login via user login

--- app/Http/Controllers/Frontend/AuthController.php
+++ app/Http/Controllers/Frontend/AuthController.php
@@ -48,7 +48,11 @@ class AuthController extends Controller
             'email'    => 'required|email',
             'password' => 'required',
         ]);
-
+        $isUser = User::where('email', $request['email'])->where('role_id', 1)->first();
+        if (!$isUser) {
+            $this->helper->one_time_message('danger', 'Please Check Your Email/Password');
+            return redirect()->route('user.login');
+        }
             if (Auth::guard('web')->attempt(['email' => trim($request['email']), 'password' => trim($request['password'])]))
             {
                 return redirect()->route('user.dashboard');
