6b99db1e9c773a8924adb3667dc8b5747ca14151
Mitesh1703 <patelmitesh173@gmail.com>
2023-04-30 10:13:39 +0000

    add transaction details

--- resources/views/frontend/dashboard/shipment_transactions.blade.php
+++ resources/views/frontend/dashboard/shipment_transactions.blade.php
@@ -28,7 +28,7 @@
                                     @php($i=1)
                                     @foreach($transactions as $transaction)
                                     <tr>
-                                       <td>{{$i}}</td>
+                                       <td>{{$transaction->id}}</td>
                                        <td>{{ \Carbon\Carbon::parse($transaction->created_at)->isoFormat('Do MMM YYYY') }}</td>
                                        <td>${{$transaction->shipping_charge}}</td>
                                        <td>{{$transaction->payment_gateway}}</td>
@@ -37,64 +37,63 @@
                                           <a href="javascript:void(0);" class="clickMeForViewDetails">View Details</a>
                                           <div class="modal fade clickMeForViewBox" id="clickMeForViewDetails{{$i}}" role="dialog" >
                                              <div class="modal-dialog modal-lg vertical-align-center" style="margin-top: 80px;">
-                                             <div class="modal-content">
-                                                <div class="modal-header">
-                                                   <h4 class="modal-title">Receipt</h4>
-                                                   <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                </div>
-                                                <div class="modal-body ">
-                                                    <div class="row">
-               <div class="col-md-12 col-lg-812">
-                  <div class="shipment-history view-transactions" style="width:100%;">
-                        <table class="table">
-                           <tbody>
-                              <tr>
-                                 <td>Order ID</td>
-                                 <td>202303050547057000000000</td>
-                              </tr>
-                              <tr>
-                                 <td>Paid Amt.</td>
-                                 <td>$200.00</td>
-                              </tr>
-                              <tr>
-                                 <td>Transaction ID</td>
-                                 <td></td>
-                              </tr>
-                              <tr>
-                                 <td>Date</td>
-                                 <td>2023-03-05T05:47:00.000Z</td>
-                              </tr>
-                              
-                              <tr>
-                                 <td>Payment Type</td>
-                                 <td>PayBaba</td>
-                              </tr>
-                              
-                              <tr>
-                                 <td>Payment Status</td>
-                                 <td>Pending</td>
-                              </tr>
-                           </tbody>
-                        </table>
-                  </div>
-               </div>
-            </div>
-
-                                                   <div class="row">
-                                                         <div class="inter-form signUpForm">
-                                                            <?php 
-                                                               $transactionData=$transaction->toArray();
-                                                            ?>
-                                                            @foreach($transactionData as $keyVal=>$transactionValue)
-                                                            <div class="row mt-4">{{ucwords(str_replace("_"," ",$keyVal))}} : {{$transactionValue}}</div>
-                                                            @endforeach
+                                                <div class="modal-content">
+                                                   <div class="modal-header">
+                                                      <h4 class="modal-title">Receipt</h4>
+                                                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                   </div>
+                                                   <div class="modal-body ">
+                                                      <div class="row">
+                                                         <div class="col-md-12 col-lg-812">
+                                                            <?php $transactionData=$transaction->toArray();?>
+                                                            <div class="shipment-history view-transactions" style="width:100%;">
+                                                               <table class="table">
+                                                                  <tbody>
+                                                                     <tr>
+                                                                        <td>Order ID</td>
+                                                                        <td><?php echo checkKeyExists("id",$transactionData);?></td>
+                                                                     </tr>
+                                                                     <tr>
+                                                                        <td>Paid Amt.</td>
+                                                                        <td>$<?php echo checkKeyExists("shipping_charge",$transactionData);?></td>
+                                                                     </tr>
+                                                                     <tr>
+                                                                        <td>Transaction ID</td>
+                                                                        <td><?php 
+                                                                           $paymentData=checkKeyExists("payment_response",$transactionData);
+                                                                           if(!empty($paymentData)){
+                                                                              $paymentData=json_decode($paymentData,true);
+                                                                              echo $paymentData['transactionid'];
+                                                                           }else{
+                                                                              echo "--";
+                                                                           }
+                                                                           ?>
+                                                                        </td>
+                                                                     </tr>
+                                                                     <tr>
+                                                                        <td>Date</td>
+                                                                        <td><?php echo date("Y-m-d h:i:s",strtotime(checkKeyExists("created_at",$transactionData)));?></td>
+                                                                     </tr>
+                                                                     
+                                                                     <tr>
+                                                                        <td>Payment Type</td>
+                                                                        <td><?php echo ucfirst(checkKeyExists("payment_gateway",$transactionData));?></td>
+                                                                     </tr>
+                                                                     
+                                                                     <tr>
+                                                                        <td>Payment Status</td>
+                                                                        <td><?php echo ucfirst(checkKeyExists("payment_status",$transactionData));?></td>
+                                                                     </tr>
+                                                                  </tbody>
+                                                               </table>
+                                                            </div>
                                                          </div>
+                                                      </div>
+                                                   </div>
+                                                   <div class="modal-footer">
+                                                      <button type="button" data-dismiss="modal" class="btn btn-default">Close</button>
                                                    </div>
                                                 </div>
-                                                <div class="modal-footer">
-                                                   <button type="button" data-dismiss="modal" class="btn btn-default">Close</button>
-                                                </div>
-                                             </div>
                                              </div>
                                        </div>
                                        </td>
