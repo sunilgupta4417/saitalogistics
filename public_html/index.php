8d3c4ccd3ca260a4df956852aeb6b5833f845ccd
sunilgupta4417 <82902183+sunilgupta4417@users.noreply.github.com>
2023-05-10 18:50:23 +0530

    Update index.php v7

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance Mode Test Rohit
+| Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
